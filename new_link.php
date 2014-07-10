799c61fcb4e0f20da6cf1e1e35e4e36dfa6bd1c1
Jonathan Galperin <jonathan@galperin.com>
2014-07-10 18:33:38 -0400

    If the movie does not yet exist, a new table is created. Also sanitzied strings.

--- new_link.php
+++ new_link.php
@@ -15,11 +15,19 @@ if (isset($_POST['add']))
 
 	$conn = mysql_connect('localhost', $username, $password);
 
+    // Sanitize input
+	function sanitize($in) {
+ 		return addslashes(htmlspecialchars(strip_tags(trim($in))));
+	}
 
-   	$name = $_POST['name'];
-   	$released = $_POST['released'];
+	$sanitzedName =  mysql_real_escape_string('name');
+
+   	$name = sanitize($_POST['name']);
+   	$name = mysql_real_escape_string($name);
+   	$released = sanitize($_POST['released']);
     $runtime = $_POST['runtime'];
     $actors = $_POST['actors'];
+    $link = $_POST['link'];
     
     $sql = "SHOW TABLES LIKE '$name'";
     
@@ -27,10 +35,10 @@ if (isset($_POST['add']))
     $retval = mysql_query($sql, $conn);
     
     if (mysql_num_rows($retval))   {
-    	echo "table already existed";
-    	mysql_close($conn);
-    } else {
-		$sql = "INSERT INTO movies (Name, Released, Runtime, Actors, created) VALUES ('$name','$released', '$runtime', '$actors', NOW())";
+        
+        echo "table already existed";
+  	
+  		$sql = "INSERT INTO $name (Name, Released, Runtime, Actors, created) VALUES ('$name','$released', '$runtime', '$actors', NOW())";
 			
 		mysql_select_db('movies');
 	
@@ -41,6 +49,48 @@ if (isset($_POST['add']))
 			die ('Could not enter data: ' . mysql_error());
 		}
 		echo "enetered data successfully\n";
+  	
+    	mysql_close($conn);
+    } else {
+		
+		$sql = "CREATE TABLE `$name`
+				(id INT NOT NULL AUTO_INCREMENT, 
+				PRIMARY KEY (id), 
+				name VARCHAR(250), 
+				released VARCHAR(250), 
+				runtime VARCHAR (50), 
+				actors VARCHAR (500), 
+				created DATETIME);
+				
+				/*CREATE TABLE $name.links 
+				($name.linkId INT NOT NULL AUTO_INCREMENT,
+				PRIMARY KEY (id),
+				link VARCHAR (500) NOT NULL			
+				);
+				
+				CREATE TABLE $name.andlinks
+				($name.Id INT NOT NULL,
+				$name.linkId INT NOT NULL,
+				CONSTRAINT ML_.$name.MovieLink PRIMARY KEY (
+					$name.Id,
+					$name.linkId
+				),
+				FOREIGN KEY ($name.Id) REFERENCES $name ($name.Id),
+				FOREIGN KEY ($name.LinkId) REFERENCES $name.links ($name.linkId)
+				);
+				
+				INSERT INTO $name (Name, Released, Runtime, Actors, created) 
+				VALUES ('$name','$released', '$runtime', '$actors', NOW());				
+				
+				
+				INSERT INTO $name.links (link) 
+				VALUES ($link)*/
+				";
+    
+    	mysql_query($sql, $conn) or die (mysql_error());
+    	
+		echo "new table created.";  
+		
 		mysql_close($conn);
 	}
 } else {
@@ -66,8 +116,13 @@ if (isset($_POST['add']))
 </tr>
 
 <tr>
-<td width = "100"> Actors</td>
+<td width = "100">Actors</td>
 <td><input name = "actors" type = "text" id = "actors"></td>
+</tr>
+
+<tr>
+<td width = "100">Link</td>
+<td><input name = "link" type = "text" id = "link"></td>
 </tr>
 
 <td width="100"> </td>

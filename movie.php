2e87e9d24878e438a4ec1252b85cb6544d6cafc6
Jonathan Galperin <jonathan@galperin.com>
2014-07-29 12:21:53 -0400

    Added styling to movie page and meta data in main site, including footer.

--- movie.php
+++ movie.php
@@ -1,7 +1,16 @@
 <html>
 
 <head>
+<meta name="title" content=""/>
+<meta name="description" content=""/>
 <title></title>
+<meta name="og:site_name" content="WatchItFree"/>
+<meta name="og:title" content=""/>
+<meta name="og:description" content=""/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script src="jquery-ui-1.8.13.custom.min.js"></script>
+<link rel='stylesheet' type='text/css' href='Moviestyle2.css' />
 </head>
 
 <body>
@@ -19,27 +28,39 @@
 	
 		if ($details -> Response == 'True')
 		{
-			echo "IMDB-ID : ".$details->imdbID.'<br>';
-			echo "Title : ".$details->Title.'<br>';
-			echo "Year : ".$details->Year.'<br>';
-			echo "Rated : ".$details->Rated.'<br>';
-			echo "Poster Image Path: ".$details->Poster.'<br>';
-			echo "<img src=\"$details->Poster\"><br>";
-			echo "Released Date: ".$details->Released.'<br>';
-			echo "Runtime : ".$details->Runtime.'<br>';
-			echo "Genre : ".$details->Genre.'<br>';
-			echo "Director : ".$details->Director.'<br>';
-			echo "Writer : ".$details->Writer.'<br>';
-			echo "Actors : ".$details->Actors.'<br>';
-			echo "Plot : ".$details->Plot.'<br>';
-			echo "Language : ".$details->Language.'<br>';
-			echo "Country : ".$details->Country.'<br>';
-			echo "Awards : ".$details->Awards.'<br>';
-			echo "Metascore : ".$details->Metascore.'<br>';
-			echo "IMDB Rating : ".$details->imdbRating.'<br>';
-			echo "IMDB Votes : ".$details->imdbVotes.'<br>';
+		echo '<div id="header">
+			<div id="Home"><a href="http://www.watchitfree.me" style="text-decoration:none;">HOME</a></div>
+			</div>
+			<div id="page-content">
+			<div class="movie-container">
+			<div id="disclaimer"><h3>Welcome to the WatchItFree Alpha stage!</h3>
+			</div>
+			<div id="movie-description">';
+		echo '<h4>' .$details->Title. '<span style="opacity: 0.8">'.$details->Year.'</span></h4>
+			<br>';
 
+		echo '<p>'.$details->Plot.'</p>
+			</div>';
+		
+		echo '<div id="movie-ratings"></div>		
+			<div id="thumbnail-box">
+			<a href="" title="'.$details->Title.' (2014)">
+			<img href="'.$details->Poster.'"/></a></div>
+			</div>';
+
+		echo '<div class="dataTableContainer">
+			<table class="dataTable">
+            <thead>
+                <tr>
+                    <th class="LinkCell"><div></div></th>
+                    <th class="voteCell oddCell"><div>Voting</div></th>
+                    <th class="qualCell"><div>Quality</div></th>
+                    <th class="ageCell oddCell"><div>Age</div></th>
+                </tr>
+            </thead>
+            <tbody>';
 			$sql = "SELECT link FROM `$name`";
+
 						
 			$retval = mysql_query($sql, $conn);
 			
@@ -50,19 +71,55 @@
 			
 			while ($row = mysql_fetch_array($retval, MYSQL_ASSOC))
 			{
-				echo "Link : {$row['link']} <br> ";
+				echo '<tr id="link_ID">
+					<td class="LinkCell">
+                    <img style="height: 16px; width: 16px;" src="http://www.google.com/s2/favicons?domain=novamov.me"/>';
+                echo "<a href=' {$row['link']} '>";
+				echo 'novamov.me</a>
+                    </td>';
+					
+                echo'<td class="voteCell oddCell qualCell">
+                    <span class="text">Ratings Unavailable</span>
+                    </td>
+                    <td class="qualCell">CAM</td>
+                    <td class="ageCell oddCell">now</td>
+                </tr>';
 			}
-			
+			echo"</tbody>
+			</table>
+			</div>
+		</div>";	
 			mysql_close($conn);
 		}
 	}
 	else {
 		echo "Movie does not exist.";
-	}
-	
+	}	
 	
 ?>
-
+<div id="footer">
+<div class="wrapper">
+<div class="bottomMenu container firstone">
+<h4>Site Links</h4>
+<ul>
+<li><a href="/keywords.html">Keywords</a></li>
+<li><a href="/genres.html">Movie Genres</a></li>
+<li><a href="/years.html">Movie Years</a></li>
+<li><a href="/tv/genres.html">TV Show Genres</a></li>
+<li><a href="/tv/years.html">TV Show Years</a></li>
+<li><a href="/trends/">Trends</a></li>
+<li><a href="/latest-watched-movies.html">Latest Watched</a></li>
+<li><a href="/terms.html">Terms of Service</a></li>
+<li><a href="/rules.html">Rules</a></li>
+<li><a href="/privacy-policy.html">Privacy Policy</a></li>
+<li><a href="/contacts.html">Contacts</a></li>
+</ul>
+</div>
+</div>
+<p>
+WatchItFree provides links to other sites on the internet and doesn't host any files itself.
+</p>
+</div>
 <body>
 
 </html>
--- new_link.php
+++ new_link.php
@@ -1,7 +1,40 @@
 <html>
 <head>
 <title>Add a New Movie</title>
-<head/>
+	<script type="text/javascript">
+		function checkLinks(){
+			var valid_domains = new Array("ovfile.com", "videozer.com", "royalvids.eu", "divxstage.eu",
+			"divxstage.net", "veevr.com", "gorillavid.com", "gorillavid.in", "megavideo", "movshare.net",
+			"zshare", "tudou", "youtube", "youku", "vidbux.com", "putlocker.com", "sockshare.com",
+			"videobb.com", "videoweed.es", "videoweed.com", "smotri.com", "fairyshare.com", "milledrive.com",
+			"divxden.com", "vidxden.com", "miloyski.com", "sina.com", "putfile.com", "novamov.com",
+			"wisevid.com", "loombo.com", "vidbux.com", "zalaa.com", "vidhog.com", "xvidstage.com",
+			"nowvideo.eu", "divxbase.com", "nosvideo.com", "vidbull.com", "mooshare.biz", "180upload.com",
+			"videobam.com", "allmyvideos.net", "modovideo.com", "vidspot.net", "vodlocker.com", "movreel.com",
+			"video.tt", "faststream.in", "vidto.me", "firedrive.com");
+			inputs = document.getElementById('host','link').elements;
+			validation = true;		
+			for(i=1; i<inputs.length -1; i++){
+				if(inputs[i].value.length > 0){
+					valid = false;
+					for(did in valid_domains){
+						if((" " + inputs[i].value).indexOf(valid_domains[did]) > 0){
+							valid = true;
+							break;
+						}
+					}					
+					if(valid == false){
+						alert("This host is not allowed: " + inputs[i].value);
+						validation = false;
+						inputs[i].focus();
+						break;
+					}
+				}
+			}			
+			return validation;
+		}
+	</script>
+</head>
 
 <body>
 <?php
@@ -52,12 +85,13 @@ if (isset($_POST['add']))
 					(id INT NOT NULL AUTO_INCREMENT, 
 					PRIMARY KEY (id), 
 					link VARCHAR(500), 
-					created DATETIME)
+					created DATETIME,
+					host CHAR(30))
 					";
 				
 			$sql_insert= "INSERT INTO `$name`
-							(link, created)
-							VALUES ('$link', NOW())";
+							(link, created, host)
+							VALUES ('$link','$host', NOW())";
 	
 			mysql_query($sql_create, $conn) or die (mysql_error());
 			mysql_query($sql_insert, $conn) or die (mysql_error());
@@ -73,9 +107,8 @@ if (isset($_POST['add']))
 
 ?>
 
-<form method ="post" action = "<?php $_PHP_SELF?>">
+<form onsubmit="return checkLinks();" method ="post" action = "<?php $_PHP_SELF?>">
 <table width="400" border = "0" cellspacing = "1" cellpadding = "2">
-
 <tr>
 <td width = "100">Movie Name</td>
 <td><input name = "name" type = "text" id = "name"></td>
@@ -84,6 +117,11 @@ if (isset($_POST['add']))
 <tr>
 <td width = "100">Link</td>
 <td><input name = "link" type = "text" id = "link"></td>
+</tr>
+
+<tr>
+<td width = "100">Host Site</td>
+<td><input name = "host" type = "text" id = "host"></td>
 </tr>
 
 <td width="100"> </td>

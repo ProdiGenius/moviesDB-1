d5247862c75fa939614ebf95cff03275103876ea
Jonathan Galperin <jonathan@galperin.com>
2014-07-30 13:25:55 -0400

    Added staging environment database connection, made composer.json file valid json.

--- datalogin.php
+++ datalogin.php
@@ -1,15 +1,22 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "yossil01";
-$dbpass = "iopghj01";
-$dbname = "yossil01_movies";
-
+//!!!-LOCAL DEVELOPMENT-!!!
 /*$dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "movies";*/
 
+//!!!-STAGING ENVIRONMENT-!!!
+$dbhost = "localhost";
+$dbuser = "bde3ebf2d90852";
+$dbpass = "b3386aac";
+$dbname = "heroku_87de99dde5d61b1";
+
+//!!!-PRODUCTION ENVIRONMENT-!!!
+/*$dbhost = "localhost";
+$dbuser = "yossil01";
+$dbpass = "iopghj01";
+$dbname = "yossil01_movies";*/
 
 $conn = mysql_connect($dbhost, $dbuser, $dbpass) or die("unable to connect to database.");
 mysql_select_db($dbname) or die ("Unable to select");

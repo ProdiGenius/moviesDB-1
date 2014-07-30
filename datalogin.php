c78efd02f65c62a064db2f2d5b90f4a1f3427886
Jonathan Galperin <jonathan@galperin.com>
2014-07-30 14:00:44 -0400

    Changed staging environment db connect from msql to mysqli for ClearDb.

--- datalogin.php
+++ datalogin.php
@@ -14,6 +14,7 @@ $dbuser = $url["bde3ebf2d90852"];
 $dbpass = $url["b3386aac"];
 $dbname = substr($url["heroku_87de99dde5d61b1"], 1);
 
+mysqli_connect($dbhost, $dbuser, $dbpass);
 mysql_select_db($dbname);
 
 //!!!-PRODUCTION ENVIRONMENT-!!!
@@ -22,6 +23,6 @@ $dbuser = "yossil01";
 $dbpass = "iopghj01";
 $dbname = "yossil01_movies";*/
 
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die("unable to connect to database.");
-mysql_select_db($dbname) or die ("Unable to select");
+/*$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die("unable to connect to database.");
+mysql_select_db($dbname) or die ("Unable to select");*/
 ?>

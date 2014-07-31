cd20371bbfcdf2ceb3d9880ea4796889aea38fa7
Jonathan Galperin <jonathan@galperin.com>
2014-07-31 14:54:06 -0400

    Changed user agent.

--- movie.php
+++ movie.php
@@ -36,48 +36,21 @@
 
         $name = urlencode($name);
 
-        function curl($url) {
-            // Assigning cURL options to an array
-            $options = Array(
-                CURLOPT_RETURNTRANSFER => TRUE,  // Setting cURL's option to return the webpage data
-                CURLOPT_FOLLOWLOCATION => TRUE,  // Setting cURL to follow 'location' HTTP headers
-                CURLOPT_AUTOREFERER => TRUE, // Automatically set the referer where following 'location' HTTP headers
-                CURLOPT_CONNECTTIMEOUT => 120,   // Setting the amount of time (in seconds) before the request times out
-                CURLOPT_TIMEOUT => 120,  // Setting the maximum amount of time for cURL to execute queries
-                CURLOPT_MAXREDIRS => 10, // Setting the maximum number of redirections to follow
-                CURLOPT_USERAGENT => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1a2pre) Gecko/2008073000 Shredder/3.0a2pre ThunderBrowse/3.2.1.8",  // Setting the useragent
-                CURLOPT_URL => $url, // Setting cURL's URL option with the $url variable passed into the function
-            );
-
-            $ch = curl_init();  // Initialising cURL
-            curl_setopt_array($ch, $options);   // Setting cURL's options using the previously assigned array data in $options
-            $data = curl_exec($ch); // Executing the cURL request and assigning the returned data to the $data variable
-            curl_close($ch);    // Closing cURL
-            return $data;   // Returning the data from the function
-        }
-
-        ini_set('max_execution_time', 600);
+        $tomatoe = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=fdwz6apy246kx4nn8sy4hgda&q=$name+3&page_limit=1";
 
         if (false !== ($json = file_get_contents("http://www.omdbapi.com/?t=$name"))) {
             $details = json_decode($json);
 
             if ($details->Response == 'True') {
 
-                $homepage = file_get_contents("http://www.imdbapi.com/?t=$name");
-                $arr = json_decode($homepage, true);
-
-                $results = curl("http://www.imdbapi.com/?t=$name");
-
-                echo $results;
-
-                $results = json_decode($results);
+                $tomatoe = json_decode($tomatoe);
 
                 echo('
 
                 <div id="thumbnail-box">
 
                 <a href="" title="' . $details->Title . ' (2014)">
-			    <img src=' . $results->Poster . '/></a>
+			    <img src=' . $tomatoe->posters->profile . '/></a>
 
 			    </div>
 

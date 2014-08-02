2494b8876eef846b80a73a224ed4b00712aa010a
Jonathan Galperin <jonathan@galperin.com>
2014-08-02 17:47:31 -0400

    Uppercase words

--- index.php
+++ index.php
@@ -165,7 +165,7 @@
 
 			while ($counter < 10 && $row = mysqli_fetch_array($rs))
 			{
-                echo urldecode("<li><a class='mainLink' href='movie.php?id=$row[0]'>$row[0]</a>");
+                echo urldecode("<li><a class='mainLink' href='movie.php?id=$row[0]'>ucwords($row[0])</a>");
                 $counter++;
 			}
 

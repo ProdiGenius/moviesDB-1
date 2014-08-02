bcef7216db4eda040170eb5a29cfdb6d86fcab8d
Jonathan Galperin <jonathan@galperin.com>
2014-08-02 11:28:22 -0400

    Fixed index.php connect

--- datalogin.php
+++ datalogin.php
@@ -15,9 +15,9 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"],1);
 
-$link = mysqli_connect($server, $username, $password, $db);
+$link = mysqli_connect($server, $username, $password) or die("Unable to connect to DB.");
 
-mysqli_select_db($link, $db);
+mysqli_select_db($link, $db) or die("UNABLE to connect to db.");
 
 //!!!-PRODUCTION ENVIRONMENT-!!!
 /*$dbhost = "localhost";

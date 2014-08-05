4b51d5e6871584e54f0f06f7e613c5cd78396035
Jonathan Galperin <jonathan@galperin.com>
2014-08-04 22:14:33 -0400

    video.php page

--- video.php
+++ video.php
@@ -1,45 +1,70 @@
-<?php
+<html>
 
-include ("datalogin.php");
+<head>
+    <link rel='stylesheet' type ='text/css' href='css/style.css' />
+    <link rel="stylesheet" type="text/css" href="css/search.css" media="screen" />
+</head>
 
-$name = $_GET["name"];
+<body>
 
-$link_id = $_GET["id"];
+<div id="header">
 
-$name = urlencode($name);
+    <div id="navContainer">
 
-//echo("Movie: " . $name . " id: " . $link_id);
+        <div id="Home"><a href="http://www.watchitfree.me" style="text-decoration:none;">HOME</a></div>
 
-function addhttp($url)
-{
-    if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
-        $url = "http://" . $url;
-    }
-    return $url;
-}
+        <div id="Home"><a href="new_link.php" style="text-decoration:none;">LINKS</a></div>
 
-$sql = "SELECT link FROM `$name` WHERE id='$link_id'";
-
-$response = mysqli_query($conn, $sql);
-
-if (!$response)
-{
-    die ("Could not get data.");
-}
-
-$row = mysqli_fetch_assoc($response);
-
-$video_link = $row["link"];
-$video_link = addhttp($video_link);
-
-echo ('
-
-    <div id="video_container">
-
-        <h1><a href="'.$video_link.'">CLICK HERE TO PROCEED TO THE VIDEO</a></h1>
-
+        <div id="search">
+            <div class="icon">
+                <p>SEARCH:</p>
+            </div>
+            <input id="search" type="text" autocomplete="off">
+            <ul id="results"></ul>
+        </div>
     </div>
 
-');
+</div>
 
-?>
+
+<div id="video_container">
+    <?php
+
+    include ("datalogin.php");
+
+    $name = $_GET["name"];
+
+    $link_id = $_GET["id"];
+
+    $name = urlencode($name);
+
+    //echo("Movie: " . $name . " id: " . $link_id);
+
+    function addhttp($url)
+    {
+        if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+            $url = "http://" . $url;
+        }
+        return $url;
+    }
+
+    $sql = "SELECT link FROM `$name` WHERE id='$link_id'";
+
+    $response = mysqli_query($conn, $sql);
+
+    if (!$response)
+    {
+        die ("Could not get data.");
+    }
+
+    $row = mysqli_fetch_assoc($response);
+
+    $video_link = $row["link"];
+    $video_link = addhttp($video_link);
+
+    echo ('<h1><a href="'.$video_link.'">CLICK HERE TO PROCEED TO THE VIDEO</a></h1>');
+
+    ?>
+</div>
+</body>
+</html>
